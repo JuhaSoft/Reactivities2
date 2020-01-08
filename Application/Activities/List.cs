577d5ae3eccc9dde255d1b53e1a84ad362407f4b
Juha Way <gugai.way@gmail.com>
2020-01-08 16:10:28 +0700

    8-1-2020

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -13,25 +13,25 @@ namespace Application.Activities {
         public class Query : IRequest<List<Activity>> { }
         public class Handler : IRequestHandler<Query, List<Activity>> {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
+            // private readonly ILogger<List> _logger;
 
-            public Handler (DataContext context, ILogger<List> logger) {
-                _logger = logger;
+            public Handler (DataContext context) {
+                // _logger = logger;
                 _context = context;
             }
 
             public async Task<List<Activity>> Handle (Query request, CancellationToken cancellationToken) {
-                try {
-                    for (var i = 0; i < 10; i++) {
-                        cancellationToken.ThrowIfCancellationRequested ();
-                        await Task.Delay (1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has Completed");
-                    }
-                }
-                catch (Exception ex) when (ex is TaskCanceledException)
-                {
-                    _logger.LogInformation("Task was cancelled");
-                }
+                // try {
+                //     for (var i = 0; i < 10; i++) {
+                //         cancellationToken.ThrowIfCancellationRequested ();
+                //         await Task.Delay (1000, cancellationToken);
+                //         _logger.LogInformation($"Task {i} has Completed");
+                //     }
+                // }
+                // catch (Exception ex) when (ex is TaskCanceledException)
+                // {
+                //     _logger.LogInformation("Task was cancelled");
+                // }
 
 
 
